2362c440d9a84c1e64a8d7e13bd8197e480a178b
Dominique Youness <youness.dominique@gmail.com>
2021-05-02 22:23:14 +0200

    Splash Screen config oklm

---
+++ app/src/main/java/com/example/android_jetpack/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.android_jetpack
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.example.android_jetpack.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater);
+        setContentView(splashBinding.root)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        //splashBinding.tvAppName.text = "Hello world"
+    }
+}
